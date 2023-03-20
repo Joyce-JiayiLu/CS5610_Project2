2a82fe9d9a6ee904ba10f608061065fecf7b8605
jiayilu <lu.jiayi2@northeastern.edu>
2023-03-20 00:24:07 -0700

    Revert "deploy config"
    This reverts commit d9324a6d36786a84fe59777e87994c68fc59e8e3.

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const browserRouter = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/rule", element: <Rule /> },
-  { path: "/game/:difficulty", element: <Game /> },
+  { path: "/game/*", element: <Game /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
